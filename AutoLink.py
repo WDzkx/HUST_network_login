d4962ac7d056b6a962b98da9a917d8c11ca2289d
WDzkx <167607633+WDzkx@users.noreply.github.com>
2025-09-03 16:40:25 +0800

    Create AutoLink.py

---
+++ AutoLink.py
@@ -0,0 +1,28 @@
+import requests
+import logging
+import http.client
+
+# 设置请求URL
+url = 'http://172.18.18.61:8080/eportal/InterFace.do?method=login'
+
+# 设置请求数据
+    # 将userId中的example替换为自己的学号，例:U2023XXXXX
+    # 将password中的example替换为自己的校园网密码
+    # 将queryString中的example替换为自己的校验码(需要在登录认证网页手动抓取)
+    # 其他无需更改
+data = {
+    "userId": "example",      # 学号
+    "password": "example",    # 密码
+    "service": "",
+    "queryString": "example",  # 校验码
+    "operatorPwd": "",
+    "operatorUserId": "",
+    "validcode": "",
+    "passwordEncrypt": "true",
+}
+
+# 发送 POST 请求
+response = requests.post(url=url, data=data)
+
+# 打印响应状态码
+print(response.status_code)
